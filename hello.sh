eff470853c4e3062a995acebf5aaefc034278c8f
muthuifet <142093314+muthuifet@users.noreply.github.com>
2023-12-17 00:04:19 +0530

    Update hello.sh
    Hello team there was a bug and now its fixed

--- hello.sh
+++ hello.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "Hello all and Good morning welcome to the seassion"
 echo "Be Regular to the session" 
+#!/bin/bash
+echo "Hello all and Good morning welcome to the seassion"
+echo "Be Regular to the session"
+echo "Hello all and Good morning welcome to the seassion"
+echo "Be Regular to the session"
+echo "Hello all and Good morning welcome to the seassion"
+echo "Be Regular to the session"
